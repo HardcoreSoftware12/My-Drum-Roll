aaedf8e519669b41c8dfe4b5dde2aa814b953103
HardcoreSoftware12 <devadigaranjith12@gmail.com>
2023-10-02 13:05:35 +0530

    last commit

--- index.js
+++ index.js
@@ -13,11 +13,68 @@ for(var i=0; i<numOfDrumButtons ; i++){
         // audio.play();
         console.log(this.textContent)
         // this.style.color = 'white'
+        makeSound(this.textContent)
+        buttonAnimation(this.textContent)
         
+
         
     })
-    
 
+}
+
+window.addEventListener('keydown',function(event){
+    // console.log(event.key);
+    makeSound(event.key)
+    buttonAnimation(event.key)
+})
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3")
+            audio.play();
+            break;
+
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3")
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3")
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3")
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3")
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3")
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3")
+            audio.play();
+            break;
+        default:
+            console.log('default case');
+        
+    }
+
+}
+function buttonAnimation(key){
+    console.log(key);
+    var activeButton = document.querySelector(`.${key}`)
+    activeButton.classList.add('pressed')
+    // console.log(activeButton.classList);
+    setTimeout(()=>{
+        activeButton.classList.remove('pressed')
+
+    },100)
+    
 
 }
 
